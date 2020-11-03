5b3dbfe169e40b8b73ceae6bcb4f960aa8127040
Thais Martins <contato@thaismartins.co>
2020-11-03 12:50:27 -0300

    adding error page

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,8 @@
-import { Head } from 'next/head'
 import { ContentCenter, Title } from '../styles/global'
 
 const Error = () => {
   return (
     <>
-      <Head>
-        <title>Error 404 | Exponencial</title>
-      </Head>
       <ContentCenter>
         <Title>404</Title>
       </ContentCenter>
